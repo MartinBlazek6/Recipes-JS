cd2d99cfe2ff9fe2d94e979b4979704530370ff5
Martin Blažek <89529362+MartinBlazek6@users.noreply.github.com>
2024-06-10 17:45:57 +0200

    Update RecipesWorkingApplicationTests.java
    test fix

--- src/test/java/org/example/recipesworking/RecipesWorkingApplicationTests.java
+++ src/test/java/org/example/recipesworking/RecipesWorkingApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.recipesworking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RecipesWorkingApplicationTests.class)
 class RecipesWorkingApplicationTests {
 
     @Test
